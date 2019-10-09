521484d1d5e818793ebf0fd0a7dab11b247b953e
Andrea Longo <andrea.longo@caffeina.com>
2019-10-09 22:57:23 +0200

    feat: Added automerge_level option

--- MaliniAeria/Decorators/WithAeria.php
+++ MaliniAeria/Decorators/WithAeria.php
@@ -14,16 +14,31 @@ class WithAeria extends PostDecorator implements PostDecoratorInterface
         $wp_post = $post->wp_post;
 
         $aeria_automerge_fields = $this->getConfig('automerge_fields', $options, false);
+        $aeria_automerge_level = $this->getConfig('automerge_level', $options, 1);
+
+        // accept only 1 or 2
+        $aeria_automerge_level = max(1, min(2, $aeria_automerge_level));
 
         if ($aeria_automerge_fields) {
+            $post_fields = AeriaAccessor::getPostFields($wp_post);
             $fields_keys = array_keys(
-                AeriaAccessor::getPostFields($wp_post)
+                $post_fields
             );
+
             foreach ($fields_keys as $key) {
-                $post->addAttribute(
-                    $key,
-                    '@aeria:' . $key
-                );
+                if ($aeria_automerge_level == 2) {
+                    foreach ($post_fields[$key] as $sub_key => $_element) {
+                        $post->addAttribute(
+                            $sub_key,
+                            '@aeria:' . $key . ',' . $sub_key
+                        );
+                    }
+                } else {
+                    $post->addAttribute(
+                        $key,
+                        '@aeria:' . $key
+                    );
+                }
             }
             $this->filterConfig(
                 $post,
